16a3991776b3c5afcb60d6dc74f8de3dad9b60fb
Azad Johari <azad@xpronet.it>
2021-10-16 10:18:10 +0800

    updated navbar.js

--- tableapp-vue/js/navbar.js
+++ tableapp-vue/js/navbar.js
@@ -1,5 +1,5 @@
-Vue.component("navbar",{
-    template:`
+Vue.component("navbar", {
+    template: `
     <div class="row">
         <div class="col">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -12,19 +12,47 @@ Vue.component("navbar",{
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="book.html">Book Table</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="list.html">View Bookings</a>
+                        <li v-for="m in menu" class="nav-item">
+                            <a class="nav-link" v-bind:href="m.linkUrl" v-bind:target="openNewTab(m.newTab)">{{m.linkText}}</a>
                         </li>
                     </ul>
                 </div>
             </nav>
         </div>
     </div>
-    `
+    `,
+    data: function () {
+        return {
+            menu: [
+                {
+                    linkUrl: "index.html",
+                    linkText: "Home"
+                },
+                {
+                    linkUrl: "book.html",
+                    linkText: "Book Table"
+                },
+                {
+                    linkUrl: "list.html",
+                    linkText: "View Booking"
+                },
+                {
+                    linkUrl: "https://www.metallica.com",
+                    linkText: "Metallica",
+                    newTab: true
+                }
+            ]
+        }
+    },
+    methods: {
+        openNewTab: function (open) {
+            let _target = ""
+            if(open){
+                _target = "_blank"
+            } else {
+                _target = "_self"
+            }
+            return _target
+        }
+    }
 })
