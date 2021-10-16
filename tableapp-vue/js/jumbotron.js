3c151d859cab9c09af84d1c82c57abae1f09364e
Azad Johari <azad@xpronet.it>
2021-10-16 10:05:44 +0800

    init tableapp-vue and convertindex.html

---
+++ tableapp-vue/js/jumbotron.js
@@ -0,0 +1,14 @@
+Vue.component("jumbotron", {
+    template: `
+    <div class="row">
+        <div class="col">
+            <div class="jumbotron">
+                <h1 class="display-4">Discover your taste palates!</h1>
+                <p class="lead">With thousands menu to choose from, the only thing you need is your gut!</p>
+                <hr class="my-4">
+                <a class="btn btn-primary btn-lg" href="book.html" role="button">I'm ready, lets do this!</a>
+            </div>
+        </div>
+    </div>
+    `
+})
